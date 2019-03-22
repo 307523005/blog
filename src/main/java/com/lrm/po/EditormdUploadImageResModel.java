898b4dccf578030d5bf4c798b382b96117d27ebf
307523005 <307523005@qq.com>
2019-03-22 16:04:12 +0800

    钮豪 博客

---
+++ src/main/java/com/lrm/po/EditormdUploadImageResModel.java
@@ -0,0 +1,39 @@
+package com.lrm.po;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/3/4 14:58
+ * @Description:
+ */
+public class EditormdUploadImageResModel {
+    private Integer success;
+
+    private String message;
+
+    private String url;
+
+    public Integer getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Integer success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
--- src/main/java/com/lrm/web/admin/BlogController.java
+++ src/main/java/com/lrm/web/admin/BlogController.java
@@ -1,25 +1,35 @@
 package com.lrm.web.admin;
 
+import com.google.common.io.ByteStreams;
 import com.lrm.po.Blog;
-import com.lrm.po.Type;
+import com.lrm.po.EditormdUploadImageResModel;
 import com.lrm.po.User;
 import com.lrm.service.BlogService;
 import com.lrm.service.TagService;
 import com.lrm.service.TypeService;
 import com.lrm.vo.BlogQuery;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by limi on 2017/10/15.
@@ -74,10 +84,9 @@ public class BlogController {
         setTypeAndTag(model);
         Blog blog = blogService.getBlog(id);
         blog.init();
-        model.addAttribute("blog",blog);
+        model.addAttribute("blog", blog);
         return INPUT;
     }
-
 
 
     @PostMapping("/blogs")
@@ -87,12 +96,12 @@ public class BlogController {
         blog.setTags(tagService.listTag(blog.getTagIds()));
         Blog b;
         if (blog.getId() == null) {
-            b =  blogService.saveBlog(blog);
+            b = blogService.saveBlog(blog);
         } else {
             b = blogService.updateBlog(blog.getId(), blog);
         }
 
-        if (b == null ) {
+        if (b == null) {
             attributes.addFlashAttribute("message", "操作失败");
         } else {
             attributes.addFlashAttribute("message", "操作成功");
@@ -102,12 +111,132 @@ public class BlogController {
 
 
     @GetMapping("/blogs/{id}/delete")
-    public String delete(@PathVariable Long id,RedirectAttributes attributes) {
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
         blogService.deleteBlog(id);
         attributes.addFlashAttribute("message", "删除成功");
         return REDIRECT_LIST;
     }
 
+     @RequestMapping(value = "/editormd/uploadImage"/*, method = RequestMethod.POST*/)
+    @ResponseBody
+    //@PostMapping("/editormd/uploadImage")
+    public EditormdUploadImageResModel editormduploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        EditormdUploadImageResModel res = new EditormdUploadImageResModel();
+        res.setSuccess(0);
+        ServletContext application = request.getSession().getServletContext();
+        String savePath = application.getRealPath("/") + "images22/";
+        User user = (User) session.getAttribute("user");
+        //String savePath ="d:/niuhao-images/"+user.getUsername()+"/images/";
+        // 文件保存目录URL
+        String saveUrl = request.getContextPath() + "/images22/";
+        // 定义允许上传的文件扩展名
+        HashMap<String, String> extMap = new HashMap<String, String>();
+        String dirName = user.getUsername();
+        extMap.put(dirName, "gif,jpg,jpeg,png,bmp");
+        extMap.put("flash", "swf,flv");
+        extMap.put("media", "swf,flv,mp3,wav,wma,wmv,mid,avi,mpg,asf,rm,rmvb");
+        extMap.put("file", "doc,docx,xls,xlsx,ppt,htm,html,txt,zip,rar,gz,bz2");
+
+        // 最大文件大小
+        long maxSize = 3000000;
+
+        response.setContentType("text/html; charset=UTF-8");
+
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            return res;
+
+        }
+        // 检查目录
+        File uploadDir = new File(savePath);
+        if (!uploadDir.isDirectory()) {
+            uploadDir.mkdirs();
+            // return getError("上传目录不存在。");
+        }
+        // 检查目录写权限
+        if (!uploadDir.canWrite()) {
+            return res;
+
+        }
+
+        //String dirName = request.getParameter("dir");
+
+        if (dirName == null) {
+            dirName = "image";
+        }
+        if (!extMap.containsKey(dirName)) {
+            return res;
+
+        }
+        // 创建文件夹
+        savePath += dirName + "/";
+        saveUrl += dirName + "/";
+        File saveDirFile = new File(savePath);
+        if (!saveDirFile.exists()) {
+            saveDirFile.mkdirs();
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String ymd = sdf.format(new Date());
+        savePath += ymd + "/";
+        saveUrl += ymd + "/";
+        File dirFile = new File(savePath);
+        if (!dirFile.exists()) {
+            dirFile.mkdirs();
+        }
+
+        FileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        upload.setHeaderEncoding("UTF-8");
+
+
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+
+        Iterator item = multipartRequest.getFileNames();
+        while (item.hasNext()) {
+
+            String fileName = (String) item.next();
+
+            MultipartFile file = multipartRequest.getFile(fileName);
+
+
+            // 检查文件大小
+
+            if (file.getSize() > maxSize) {
+
+                return res;
+
+
+            }
+
+            // 检查扩展名
+
+            String fileExt = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1).toLowerCase();
+
+            if (!Arrays.asList(extMap.get(dirName).split(",")).contains(fileExt)) {
+                return res;
+
+            }
+            SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+
+            String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
+
+            try {
+
+                File uploadedFile = new File(savePath, newFileName);
+
+                //保存
+                ByteStreams.copy(file.getInputStream(), new FileOutputStream(uploadedFile));
+
+                //file.transferTo(uploadedFile);
+            } catch (Exception e) {
+                return res;
+            }
+            res.setSuccess(1);
+            res.setMessage(fileName);
+            res.setUrl(saveUrl + newFileName);
+            return res;
+        }
+        return res;
+    }
 
 
 }
